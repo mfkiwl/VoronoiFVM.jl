680e8b6fdcc39bc838eb1dc730d272da709d9792
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2024-11-08 20:59:02 +0100

    Code for using LinearSolve `precs` to handle preconditioners.
    Fortunately, this can be introduced without replacing the existing
    API, so that one can be cleanly deprecated.
    Also added first deprecation warnings for the older API

--- src/vfvm_linsolve.jl
+++ src/vfvm_linsolve.jl
@@ -234,22 +234,39 @@ canonical_matrix(A::AbstractExtendableSparseMatrixCSC) = SparseMatrixCSC(A)
 
 function _solve_linear!(u, state, nlhistory, control, method_linear, A, b)
     if isnothing(state.linear_cache)
-        Pl = control.precon_linear(canonical_matrix(A))
+        if !isa(method_linear,  LinearSolve.SciMLLinearSolveAlgorithm)
+            @warn "use of $(typeof(method_linear)) is deprecated, use an algorithm from LinearSolve"
+        end
+        if hasproperty(method_linear, :precs) && !isnothing(method_linear.precs)
+            Pl=nothing
+        else
+            Pl = control.precon_linear(canonical_matrix(A))
+            if !isa(Pl, Identity) && isa(method_linear,  LinearSolve.AbstractKrylovSubspaceMethod)
+                @warn "Use of control.precon_linear is deprecated. Use the `precs` API of LinearSolve"
+            end
+        end
         nlhistory.nlu += 1
         p = LinearProblem(canonical_matrix(A), b)
         state.linear_cache = init(p,
-                                   method_linear;
-                                   abstol = control.abstol_linear,
-                                   reltol = control.reltol_linear,
-                                   maxiters = control.maxiters_linear,
-                                   verbose = doprint(control, 'l'),
-                                   Pl,)
+                                  method_linear;
+                                  abstol = control.abstol_linear,
+                                  reltol = control.reltol_linear,
+                                  maxiters = control.maxiters_linear,
+                                  verbose = doprint(control, 'l'),
+                                  Pl,)
     else
-        state.linear_cache.A = canonical_matrix(A)
-        state.linear_cache.b = b
-        if control.keepcurrent_linear
-            nlhistory.nlu += 1
-            state.linear_cache.Pl = control.precon_linear(canonical_matrix(A))
+        if hasproperty(method_linear, :precs) && !isnothing(method_linear.precs)
+            reinit!(state.linear_cache; A=canonical_matrix(A), b, reuse_precs=!control.keepcurrent_linear)
+            if control.keepcurrent_linear
+                nlhistory.nlu += 1
+            end
+        else
+            state.linear_cache.A = canonical_matrix(A)
+            state.linear_cache.b = b
+            if control.keepcurrent_linear
+                nlhistory.nlu += 1
+                state.linear_cache.Pl = control.precon_linear(canonical_matrix(A))
+            end
         end
     end
 
