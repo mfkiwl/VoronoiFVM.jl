1165da1b8b2315b76a9386bcf5176cb8dbefb6a3
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2024-12-23 20:19:22 +0100

    Add a test for eltypes of callback functions

---
+++ test/test_geomitems.jl
@@ -0,0 +1,53 @@
+module test_geomitems
+using VoronoiFVM
+using ExtendableGrids
+using ForwardDiff
+using Test
+
+const Dual64 = ForwardDiff.Dual{Float64, Float64, 1}
+
+flux(y, u, edge, data) = y[1] = u[1, 1] - u[1, 2]
+
+
+function main(;n=3)
+    g = simplexgrid(0:0.1:1)
+    sys = VoronoiFVM.System(g; flux,species = 1:n)
+    time=0.0
+    λ=0.0
+    params=zeros(3)
+
+    utest=zeros(Dual64,2*n)
+    node = VoronoiFVM.Node(sys, time, λ, params)
+    u=unknowns(node,utest)
+    @test typeof(u[1])==eltype(u)
+    @test typeof(copy(u)[1])==eltype(u)
+    @test length(u)==n
+    @test size(u)==(n,)
+
+    bnode = VoronoiFVM.BNode(sys, time, λ, params)
+    u=unknowns(bnode,utest)
+    @test typeof(u[1])==eltype(u)
+    @test typeof(copy(u)[1])==eltype(u)
+    @test length(u)==n
+    @test size(u)==(n,)
+
+    edge = VoronoiFVM.Edge(sys, time, λ, params)
+    u=unknowns(edge,utest)
+    @test typeof(u[1])==eltype(u)
+    @test typeof(copy(u)[1])==eltype(u)
+    @test length(u)==2*n
+    @test size(u)==(n,2)
+
+    bedge = VoronoiFVM.BEdge(sys, time, λ, params)
+    u=unknowns(bedge,utest)
+    @test typeof(u[1])==eltype(u)
+    @test typeof(copy(u)[1])==eltype(u)
+    @test length(u)==2*n
+    @test size(u)==(n,2)
+end
+
+function runtests()
+    main()
+    return nothing
+end
+end
