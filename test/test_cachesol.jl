5b85fc5558f34e80b0c888ec0b193bfd94fee4bb
Daniel Runge <runge@wias-berlin.de>
2025-04-03 15:42:40 +0200

    cleaned up linear solve cache solution test

--- test/test_cachesol.jl
+++ test/test_cachesol.jl
@@ -7,12 +7,12 @@ using LinearAlgebra
 using SparseArrays
 using Test
 
-function flux(f, u, edge, data)
+function flux!(f, u, edge, data)
     f[1] = u[1, 1] - u[1, 2]
     return nothing
 end
 
-function bcondition(f, u, bnode, data)
+function bcondition!(f, u, bnode, data)
     boundary_dirichlet!(f, u, bnode; species = 1, region = 1, value = 0.0)
     boundary_dirichlet!(f, u, bnode; species = 1, region = 2, value = 1.0)
     return nothing
@@ -21,18 +21,16 @@ end
 function setup_linear_solve(n)
     # define system, initialize system matrix
     X = linspace(0.0, 1.0, n)
-    system = VoronoiFVM.System(X; flux, bcondition, species = [1])
-    state = VoronoiFVM.SystemState(system)
+    system = VoronoiFVM.System(X; flux = flux!, bcondition = bcondition!, species = [1])
     F = unknowns(system; inival = 0.0)
     # assemble system matrix A and the residual F of A*U=RHS in U=0, i.e.
     # F = A*0 - RHS = -RHS
     F, A = VoronoiFVM.evaluate_residual_and_jacobian(system, F)
 
-    p = LinearProblem(A, VoronoiFVM.dofs(F))
+    p = LinearProblem(A, -VoronoiFVM.dofs(F))
     linear_cache = init(p, LinearSolve.UMFPACKFactorization())
-    linear_cache.b = -VoronoiFVM.dofs(F)
 
-    return X, linear_cache, state, A
+    return X, linear_cache
 end
 
 function solve_problem(cache)
@@ -41,7 +39,7 @@ function solve_problem(cache)
 end
 
 function main(; n = 10)
-    X, cache, _ = setup_linear_solve(n)
+    X, cache = setup_linear_solve(n)
     approx_sol = solve_problem(cache)
     true_sol = map((x) -> (x), X)
     @test norm(approx_sol .- true_sol, Inf) ≤ 3.0e-16
